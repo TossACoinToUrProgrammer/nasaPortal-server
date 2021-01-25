2dc2d1aa3d6bb5ab7f4efe26d5bb0554c0db5179
Arlen Manasov <59771669+TossACoinToUrProgrammer@users.noreply.github.com>
2021-01-25 21:24:26 +0600

    changed port to PORT

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.use(express.json({ extended: true }));
 
 app.use("/api/auth", require("./routes/auth.routes"));
 
-const PORT = process.env.port || 5000;
+const PORT = process.env.PORT || 5000;
 
 async function start() {
   try {
